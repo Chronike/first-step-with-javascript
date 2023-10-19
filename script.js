16bd36d152d9ad67a78c307dcd6c6aba8e6fae8d
Giuliano Esposito <giuliano.esposito@gmx.net>
2023-10-19 09:42:59 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Use the concatenation operator to concatenate each letter of your firstname into a string one letter by one.
+console.log("G" + "i" + "u" + "l" + "i" + "a" + "n" + "o");
+
+// Error! Make it work "console.log("Hallo";)""
+console.log("Hallo");
+
+// Error! Make it work "console.log("That doesn"t work");"
+console.log("That doesn" + "t " + "work");
+
+// should return true. Change only the operator to fix it. "console.log(50 + 2 > 52);""
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it. "console.log("333" == 333);"
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it. "console.log(600 + 510 / 2);"
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22 "console.log("2" + 2);"
+console.log(2 + 2);
+
+// Create a console.log that returns true when both division results are equal to 5
+console.log(25 / 5 == 5);
+console.log(50 / 10 == 5);
